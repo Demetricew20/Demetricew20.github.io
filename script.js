e33dc27314c82a8155054e52b91c8978bd5786fa
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-01 14:37:46 -0800

    Added media queries. Fixed errors in script.js when switching active nav links.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-"use strict";
 
 // ===== MENU ==== //
 const navMenu = document.getElementById('nav-menu'),
@@ -34,12 +33,12 @@ function scrollActive(){
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+        sectionId = current.getAttribute('id');
 
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.add('active');
+            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active');
         }else{
-            document.querySelector('.nav_menu a[href*='+sectionId+']').classList.remove('active')
-        }
-    })
+            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active');
+        };
+    });
 };
