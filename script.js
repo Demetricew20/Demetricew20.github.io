332a5a6304e059ff4ea971d442097123335851eb
DemetriceW20 <demetrice_williams@hotmail.com>
2023-12-24 10:42:56 -0600

    updated icons and projects. fixed typos.

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ toggleMenu.addEventListener('click', () => {
 // HIDE
 closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('show')
-} );
+});
 
 // ==== REMOVE MENU ==== //
 const navLink = document.querySelectorAll('.nav__link');
 
-function linkAction(){
+function linkAction() {
     navMenu.classList.remove('show')
 };
 navLink.forEach(n => n.addEventListener('click', linkAction));
@@ -26,36 +26,35 @@ const sections = document.querySelectorAll('section[id]');
 
 window.addEventListener('scroll', scrollActive);
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset;
 
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
+        const sectionTop = current.offsetTop - 100;
         sectionId = current.getAttribute('id');
-
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active');
-        }else{
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active');
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active');
         };
     });
 };
 
 // =====PROJECTS===== //
 
-function projectToggle(target){
-const projectData = document.getElementById(`projects${target}-data`),
+function projectToggle(target) {
+    const projectData = document.getElementById(`projects${target}-data`),
 
-clickEvent = ('click', () => {
-    if (projectData.style.opacity != '0'){
-    projectData.style.opacity = '0';
-    }else{
-        projectData.style.opacity = "1";
-    }
-})
+        clickEvent = ('click', () => {
+            if (projectData.style.opacity != '0') {
+                projectData.style.opacity = '0';
+            } else {
+                projectData.style.opacity = "1";
+            }
+        })
 
-clickEvent();
+    clickEvent();
 
 }
 
